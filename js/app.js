1cfa52a002593ba766b53fc02f1ed8fefc6e91bd
Ste79ITA <nastri.s@gmail.com>
2023-11-02 17:11:55 +0100

    Daily task + Bonus

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 const { createApp } = Vue;
+// const axios = require('axios');
 
 createApp({
   data() {
     return {
-      message: 'ciao',
+      mails: [],
+      api: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      message: 'vue-email-list',
+      mail: '',
     };
+  },
+  methods: {
+    createArray() {
+      for (let i = 0; i < 10; i++) {
+        axios.get(this.api).then((res) => {
+          this.mails.push(res.data.response);
+        });
+      }
+    },
+  },
+  created() {
+    this.createArray();
+    console.log(this.mails);
   },
 }).mount('#app');
